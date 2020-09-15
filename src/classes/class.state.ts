b6a204741b5e7189ae79f865ac200441d8a038f7
guilhermewebdev <guilherme@guilhermeweb.dev>
2020-09-15 01:17:57 -0300

    Implementação de histórico opcional do estado

--- src/classes/class.state.ts
+++ src/classes/class.state.ts
@@ -1,22 +1,27 @@
 interface Observer extends Function {
     (state: any): void
 }
-export default class State {
+export default class State<T> {
     private _observers: Array<Observer> = [];
-    private _state: any;
-
-    constructor(initialState: any) {
+    private _state: T;
+    private _history?: Array<T>;
+    private _useHistory?: boolean;
+    
+    constructor(initialState: T, useHistory?: boolean) {
         this._state = initialState;
+        this._useHistory = useHistory;
+        this.addToHistory(initialState);
     }
 
-    set state(state: any) {
+    set state(state: T) {
         if (state !== this._state) {
             this._state = state;
             this.notifyObservers();
+            this.addToHistory(state);
         }
     }
 
-    get state(): any {
+    get state(): T {
         return this._state;
     }
 
@@ -24,18 +29,37 @@ export default class State {
         return this._observers;
     }
 
+    get history(): Array<T> | undefined {
+        return this._history;
+    }
+
     private notifyObservers() {
         this._observers.forEach(observer => observer(this._state));
     }
 
+    private addToHistory(state: T){
+        this._useHistory && this._history?.push(state);
+    }
+
+    public clearHistory(){
+        !!this._history && (this._history = []);
+        return this;
+    }
+
+    public deleteHistory() {
+        delete this._history;
+        return this;
+    }
+
     public addObserver(observer: Observer) {
-        this._observers.indexOf(observer) === -1 && this._observers.push(observer);
+        this._observers.push(observer);
         observer(this._state)
         return this;
     }
 
     public addMultipleObservers(observers: Array<Observer>) {
         this._observers = this._observers.concat(observers);
+        observers.forEach(observer => observer(this._state));
         return this;
     }
 
